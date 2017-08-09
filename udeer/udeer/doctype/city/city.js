5656a1a25c6626863b7ab94c12a70694e84b0844
mhbu50 <mhbu50@gmail.com>
2017-08-09 21:20:12 +0000

    change some doctype

---
+++ udeer/udeer/doctype/city/city.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, moosa and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('City', {
+	refresh: function(frm) {
+
+	}
+});
---
+++ udeer/udeer/doctype/street/street.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, moosa and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Street', {
+	refresh: function(frm) {
+
+	}
+});
