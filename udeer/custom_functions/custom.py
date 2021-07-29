42e3ffeb84dc028b2c255bb930edd1eaf964e415
Ahmad <mohammad.ahmad.zulfiqar1@gmail.com>
2021-07-29 13:09:07 +0500

    2to3-3.7 compatibility

--- udeer/custom_functions/custom.py
+++ udeer/custom_functions/custom.py
@@ -1,4 +1,4 @@
-from __future__ import unicode_literals
+
 import frappe
 import frappe.utils
 from frappe.utils.oauth import get_oauth2_authorize_url, get_oauth_keys, login_via_oauth2, login_oauth_user as _login_oauth_user, redirect_post_login
@@ -164,11 +164,11 @@ def dic_to_array__property_unit(dics):
 
 @frappe.whitelist()
 def delete_role(doctype, d):
-    print '////////////////////////////'
+    print('////////////////////////////')
     admin = frappe.get_doc('User', 'Administrator')
     for role in admin.roles:
-        print role.role
+        print(role.role)
         if (role.role == doctype.name):
             admin.roles.remove(role)
-            print 'remove'
+            print('remove')
             admin.save()
--- udeer/lease_management/doctype/lease/lease.py
+++ udeer/lease_management/doctype/lease/lease.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2015, moosa and contributors
 # For license information, please see license.txt
 
-from __future__ import unicode_literals
+
 import frappe
 from frappe.model.document import Document
 from frappe.utils import flt, getdate, add_months
