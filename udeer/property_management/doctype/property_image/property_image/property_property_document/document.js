7d7f83689b8a32883e50ea1c33805ce0b22db45e
mhbu50 <mhbu50@gmail.com>
2022-03-01 22:33:20 +0300

    changes

--- udeer/property_management/doctype/property_image/property_image/property_property_document/document.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2016, moosa and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('document', {
-	refresh: function(frm) {
-
-	}
-});
--- udeer/property_management/doctype/property_image/property_image/property_property_document/document/document.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021, moosa and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Document', {
-	// refresh: function(frm) {
-
-	// }
-});
