6cff9d276f1fdbd68ee21eb0f478f22997f3f009
mhbu50 <mhbu50@gmail.com>
2022-08-01 23:50:57 +0000

    fix lease instalment

--- udeer/lease_management/doctype/lease/lease.js
+++ udeer/lease_management/doctype/lease/lease.js
@@ -2,16 +2,12 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Lease', {
-  onload: function(frm) {
+  onload: function (frm) {
     console.log("frm.doc.property_unit", frm.doc.property_unit);
     if (frm.doc.property_unit && frm.doc.__islocal) {
-      let pu = frappe.get_doc("Property Unit", frm.doc.property_unit);
-      frm.set_value("property", pu.property);
-      frm.refresh_field('property');
-      console.log("pu", pu);
-
-
+      frappe.db.get_doc("Property Unit", frm.doc.property_unit)
+        .then(doc => frm.set_value("property", doc.property),
+          frm.refresh_field('property'))
     }
-
   }
 });
--- udeer/lease_management/doctype/lease/lease.py
+++ udeer/lease_management/doctype/lease/lease.py
@@ -5,21 +5,164 @@
 
 import frappe
 from frappe.model.document import Document
-from frappe.utils import flt, getdate, add_months
+from frappe.utils import flt, getdate, add_months,cint
+
 
 
 class Lease(Document):
 
+    def validate(self):
+        monthly_rent = self.annual_rent / 12
+        lease_duration = cint(self.lease_duration.split('-')[0])
+        rent = monthly_rent * lease_duration
+        self.full_payment = (self.annual_water_usage + self.insuranse + rent) -  self.received_payment
+
     def after_insert(self):
-        installment = self.annual_rent / self.installment_number
-        for i in range(self.installment_number,0,-1):
+        monthly_rent = self.annual_rent / 12
+        lease_duration = cint(self.lease_duration.split('-')[0])
+        payment_every = cint(self.payment_every.split('-')[0])
+        due_date = self.lease_starting_date
+
+        if lease_duration == 1:
             new_installment = frappe.new_doc("Lease Installment")
             new_installment.lease = self.name
             new_installment.status = "Not Active"
-            new_installment.number = i
+            new_installment.number = i + 1  
             new_installment.due_date = add_months(self.lease_starting_date, i)
-            new_installment.amount = installment
+            new_installment.amount = monthly_rent
             new_installment.property = self.property
             new_installment.property_unit = self.property_unit
             new_installment.property_owner = self.property_owner
             new_installment.insert()
+
+        if lease_duration == 3:
+            if payment_every == 1:
+                for i in range(3):
+                    new_installment = frappe.new_doc("Lease Installment")
+                    new_installment.lease = self.name
+                    new_installment.status = "Not Active"
+                    new_installment.number = i + 1  
+                    new_installment.due_date = add_months(self.lease_starting_date, i)
+                    new_installment.amount = monthly_rent 
+                    new_installment.property = self.property
+                    new_installment.property_unit = self.property_unit
+                    new_installment.property_owner = self.property_owner
+                    new_installment.insert()
+           
+        if lease_duration == 6:
+            if payment_every == 1:
+                        for i in range(6):
+                            new_installment = frappe.new_doc("Lease Installment")
+                            new_installment.lease = self.name
+                            new_installment.status = "Not Active"
+                            new_installment.number = i + 1
+                            if( i != 0):
+                                due_date = add_months(due_date, payment_every)                        
+                            new_installment.due_date = due_date
+                            new_installment.amount = monthly_rent 
+                            new_installment.property = self.property
+                            new_installment.property_unit = self.property_unit
+                            new_installment.property_owner = self.property_owner
+                            new_installment.insert()
+            if payment_every == 3:                 
+                for i in range(2):                                                  
+                    new_installment = frappe.new_doc("Lease Installment")
+                    new_installment.lease = self.name
+                    new_installment.status = "Not Active"
+                    new_installment.number = i  + 1  
+                    if( i != 0):
+                        due_date = add_months(due_date, payment_every)
+                    new_installment.due_date = due_date
+                    new_installment.amount = monthly_rent * 3 
+                    new_installment.property = self.property
+                    new_installment.property_unit = self.property_unit
+                    new_installment.property_owner = self.property_owner
+                    new_installment.insert()
+        if lease_duration == 12:
+            if payment_every == 1:
+                        for i in range(12):
+                            new_installment = frappe.new_doc("Lease Installment")
+                            new_installment.lease = self.name
+                            new_installment.status = "Not Active"
+                            new_installment.number = i + 1  
+                            new_installment.due_date = add_months(self.lease_starting_date, i)
+                            new_installment.amount = monthly_rent 
+                            new_installment.property = self.property
+                            new_installment.property_unit = self.property_unit
+                            new_installment.property_owner = self.property_owner
+                            new_installment.insert()
+            if payment_every == 3: 
+
+                for i in range(4):                                                  
+                    new_installment = frappe.new_doc("Lease Installment")
+                    new_installment.lease = self.name
+                    new_installment.status = "Not Active"
+                    new_installment.number = i + 1
+                    if( i != 0):
+                        due_date = add_months(due_date, payment_every)
+                    new_installment.due_date = due_date
+                    new_installment.amount = monthly_rent * 3 
+                    new_installment.property = self.property
+                    new_installment.property_unit = self.property_unit
+                    new_installment.property_owner = self.property_owner
+                    new_installment.insert()
+            if payment_every == 6: 
+
+                for i in range(2):                                                  
+                    new_installment = frappe.new_doc("Lease Installment")
+                    new_installment.lease = self.name
+                    new_installment.status = "Not Active"
+                    new_installment.number = i + 1  
+                    if( i != 0):
+                        due_date = add_months(due_date, payment_every)
+                    new_installment.due_date = due_date
+                    new_installment.amount = monthly_rent * 6 
+                    new_installment.property = self.property
+                    new_installment.property_unit = self.property_unit
+                    new_installment.property_owner = self.property_owner
+                    new_installment.insert()
+        if lease_duration == 24:
+            if payment_every == 1:
+                        for i in range(24):
+                            new_installment = frappe.new_doc("Lease Installment")
+                            new_installment.lease = self.name
+                            new_installment.status = "Not Active"
+                            new_installment.number = i + 1
+                            if( i != 0):
+                                due_date = add_months(due_date, payment_every)  
+                            new_installment.due_date = due_date
+                            new_installment.amount = monthly_rent 
+                            new_installment.property = self.property
+                            new_installment.property_unit = self.property_unit
+                            new_installment.property_owner = self.property_owner
+                            new_installment.insert()
+            if payment_every == 3: 
+
+                for i in range(8):                                                  
+                    new_installment = frappe.new_doc("Lease Installment")
+                    new_installment.lease = self.name
+                    new_installment.status = "Not Active"
+                    new_installment.number = i + 1  
+                    if( i != 0):
+                        due_date = add_months(due_date, payment_every) 
+                    new_installment.due_date = due_date
+                    new_installment.amount = monthly_rent * 3 
+                    new_installment.property = self.property
+                    new_installment.property_unit = self.property_unit
+                    new_installment.property_owner = self.property_owner
+                    new_installment.insert()
+            if payment_every == 6: 
+
+                for i in range(4):                                                  
+                    new_installment = frappe.new_doc("Lease Installment")
+                    new_installment.lease = self.name
+                    new_installment.status = "Not Active"
+                    new_installment.number = i + 1 
+                    if( i != 0):
+                        due_date = add_months(due_date, payment_every) 
+                    new_installment.due_date = due_date
+                    new_installment.amount = monthly_rent * 6 
+                    new_installment.property = self.property
+                    new_installment.property_unit = self.property_unit
+                    new_installment.property_owner = self.property_owner
+                    new_installment.insert()                        
